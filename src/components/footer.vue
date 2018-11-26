d1c6ea3e3811a4b7fb23ffec031c2c66ac71b957
chenzhe512 <chenzhe512@gmail.com>
2018-11-26 14:15:15 +0800

    源码提交

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>© chenzhe512@gmail.com 2018 wechat: chenzhe512</p>
+  </footer>
+</template>
+
+<style scoped>
+footer{
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
