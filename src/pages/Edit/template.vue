b88ce34624a2714e09a7422dc81628e57f9cdea6
chenzhe512 <chenzhe512@gmail.com>
2018-11-26 15:07:00 +0800

    http>https

--- src/pages/Edit/template.vue
+++ src/pages/Edit/template.vue
@@ -7,7 +7,7 @@
   <h3>内容简介</h3>
   <el-input type="textarea" v-model="description" :autosize="{ minRows: 2, maxRows: 6}"></el-input>
   <p class="msg">限30个字</p>
-  <h3>文章标题</h3>
+  <h3>文章内容</h3>
   <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30}"></el-input>
   <p class="msg">限30个字</p>
   <p>
